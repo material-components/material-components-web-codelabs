737f4fdb55f59adc17d2bf55bc8432cc4ed1ea6c
Andy Dvorak <advorak@google.com>
2018-04-09 13:53:06 -0700

    chore: Use `includePaths` instead of `importer` in `sass-loader` config

--- mdc-112/starter/webpack.config.js
+++ mdc-112/starter/webpack.config.js
@@ -19,14 +19,7 @@ module.exports = [
           {
             loader: 'sass-loader',
             options: {
-              importer: function(url, prev) {
-                if(url.indexOf('@material') === 0) {
-                  var filePath = url.split('@material')[1];
-                  var nodeModulePath = `./node_modules/@material/${filePath}`;
-                  return { file: require('path').resolve(nodeModulePath) };
-                }
-                return { file: url };
-              }
+              includePaths: ['./node_modules'],
             }
           },
         ]
