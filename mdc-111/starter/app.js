918f67b2ce7d8966088896bae33ee1038ae4d92a
Andrew C. Dvorak <acdvorak@gmail.com>
2018-04-11 12:12:39 -0700

    WIP: Simplify form validation

--- mdc-111/starter/app.js
+++ mdc-111/starter/app.js
@@ -1,33 +1,5 @@
 const shippingForm = document.querySelector('#crane-shipping-form');
-const zipCodeInput = document.querySelector('#crane-zip-code-input');
-const submitButton = document.querySelector('#crane-submit-button');
-
 shippingForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  if (/^[0-9]{5}$/.test(zipCodeInput.value)) {
-    handleValidZipCode();
-  } else {
-    handleInvalidZipCode();
-  }
-});
-
-zipCodeInput.addEventListener('input', hideZipCodeValidationMessage);
-
-function handleValidZipCode() {
-  hideZipCodeValidationMessage();
   alert('Success!');
-}
-
-function handleInvalidZipCode() {
-  zipCodeInput.setCustomValidity('Please enter a valid 5-digit ZIP code (e.g., 94043)');
-
-  // Force browsers to display the validation message bubble over the input
-  setTimeout(() => {
-    submitButton.click();
-  });
-}
-
-function hideZipCodeValidationMessage() {
-  zipCodeInput.setCustomValidity('');
-}
+});
