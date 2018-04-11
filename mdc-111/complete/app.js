fdd14773ed55f844b01878bcbca2e81ac4550d41
Andrew C. Dvorak <acdvorak@gmail.com>
2018-04-11 15:24:43 -0700

    WIP: Simplify "complete" app.js

--- mdc-111/complete/app.js
+++ mdc-111/complete/app.js
@@ -1,24 +1,17 @@
-const MDCRipple = require('@material/ripple').MDCRipple;
-const MDCSelect = require('@material/select').MDCSelect;
-const MDCTextField = require('@material/textfield').MDCTextField;
-
-const buttonElements = [].slice.call(document.querySelectorAll('.mdc-button'));
-buttonElements.forEach((buttonEl) => {
-  MDCRipple.attachTo(buttonEl);
-});
-
-const selectElements = [].slice.call(document.querySelectorAll('.mdc-select'));
-selectElements.forEach((selectEl) => {
-  MDCSelect.attachTo(selectEl);
-});
-
-const textFieldElements = [].slice.call(document.querySelectorAll('.mdc-text-field'));
-textFieldElements.forEach((textFieldEl) => {
-  MDCTextField.attachTo(textFieldEl);
-});
-
 const shippingForm = document.querySelector('#crane-shipping-form');
 shippingForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   alert('Success!');
+});
+
+const MDCRipple = require('@material/ripple').MDCRipple;
+const MDCSelect = require('@material/select').MDCSelect;
+const MDCTextField = require('@material/textfield').MDCTextField;
+
+MDCRipple.attachTo(document.querySelector('.mdc-button'));
+MDCSelect.attachTo(document.querySelector('.mdc-select'));
+
+const textFieldElements = [].slice.call(document.querySelectorAll('.mdc-text-field'));
+textFieldElements.forEach((textFieldEl) => {
+  MDCTextField.attachTo(textFieldEl);
 });
