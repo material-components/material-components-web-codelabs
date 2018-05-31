f7b8c06555f9d5c4541a362468844fdf05afde55
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2018-05-31 17:51:53 -0400

    fix: Update webpack configs to match use of babel-preset-env (#40)

--- mdc-111/starter/webpack.config.js
+++ mdc-111/starter/webpack.config.js
@@ -46,7 +46,7 @@ module.exports = [
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['env']
         }
       }]
     },
