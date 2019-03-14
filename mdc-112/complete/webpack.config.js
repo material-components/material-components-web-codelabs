7d971ccbf2b3fdb4aa6b133319ede1dc830afde8
Kenneth G. Franqueiro <kfranqueiro@google.com>
2019-03-14 18:22:32 -0400

    chore: Update dependencies for MDC-112

--- mdc-112/complete/webpack.config.js
+++ mdc-112/complete/webpack.config.js
@@ -20,6 +20,8 @@ module.exports = [
             loader: 'sass-loader',
             options: {
               includePaths: ['./node_modules'],
+              implementation: require('dart-sass'),
+              fiber: require('fibers'),
             }
           },
         ]
